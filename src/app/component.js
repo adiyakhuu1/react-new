5b89774dc67f3f0a48a675676923de329bf43205
adiyakhuu1 <my.as.glpzghoo@gmail.com>
2024-11-27 16:56:50 +0800

    at school 11-27

--- src/app/component.js
+++ src/app/component.js
@@ -1,46 +1,49 @@
 const MyComponent = (props) => {
+  // console.log(props.props.firstName);
   return (
     <div className="cards">
-      <div className="front" id={`front${props.id}`}>
+      <div className="front" id={`front${props.props.id}`}>
         <p
           style={{ position: "absolute" }}
           className="first-name"
-          id={`firstName${props.id}`}>
-          {props.firstName}
+          id={`firstName${props.props.id}`}>
+          {props.props.firstName}
         </p>
         <p
           style={{ position: "absolute" }}
           className="last-name"
-          id={`lastName${props.id}`}>
-          {props.lastName}
+          id={`lastName${props.props.id}`}>
+          {props.props.lastName}
         </p>
         <p
           style={{ position: "absolute" }}
           className="email"
-          id={`email${props.id}`}>
-          {props.email}
+          id={`email${props.props.id}`}>
+          {props.props.email}
         </p>
         <h3
           style={{ position: "absolute" }}
           className="companyName"
-          id={`companyName${props.id}`}>
-          {props.companyName}
+          id={`companyName${props.props.id}`}>
+          {props.props.companyName}
         </h3>
         <p
           style={{ position: "absolute" }}
           className="phoneNumber"
-          id={`phoneNumber${props.id}`}>
-          {props.phoneNumber}
+          id={`phoneNumber${props.props.id}`}>
+          {props.props.phoneNumber}
         </p>
         <p
           style={{ position: "absolute" }}
           className="address"
-          id={`address${props.id}`}>
-          {props.address}
+          id={`address${props.props.id}`}>
+          {props.props.address}
         </p>
+        {/* {console.log(props)} */}
       </div>
       {/* <div className="back" id={`back${card.id}`}></div> */}
     </div>
   );
 };
 export { MyComponent };
+// MyComponent();
--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,7 @@
 import userData from "./userdata.json";
 import "./style.css";
 import { MyComponent } from "./component.js";
-const users = userData.map((props) => {
-  return <MyComponent />;
-});
+const users = userData.map((user) => <MyComponent props={user} />);
 
 export default function Home() {
   return users;
